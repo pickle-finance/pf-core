fabe15b5298d1b80b7b58730ec92616efad1f602
Rob Stryker <rob@oxbeef.net>
2021-08-05 01:07:41 -0400

    Compile error in tests
    Signed-off-by: Rob Stryker <rob@oxbeef.net>

--- tests/harvest/JarHarvestResolverDiscoveryTest.ts
+++ tests/harvest/JarHarvestResolverDiscoveryTest.ts
@@ -1,13 +1,13 @@
 import { Chain } from "../../src/chain/ChainModel";
 import { JarHarvestResolver } from "../../src/harvest/JarHarvestResolver";
 import { JarHarvestResolverDiscovery } from "../../src/harvest/JarHarvestResolverDiscovery";
-import { jars } from "../../src/model/JarsAndFarms";
+import { JAR_DEFINITIONS } from "../../src/model/JarsAndFarms";
 import { AssetEnablement } from "../../src/model/PickleModelJson";
 
 describe('Discovery for jar harvest data', () => {
 
   test('Check all enabled jars have a resolver', async () => {
-    const jars2 = jars.filter((element) => { 
+    const jars2 = JAR_DEFINITIONS.filter((element) => { 
       return element.enablement === AssetEnablement.ENABLED && element.chain === Chain.Ethereum
     });
     for( let i = 0; i < jars2.length; i++ ) {
