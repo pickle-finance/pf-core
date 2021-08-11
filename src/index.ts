9d6f8b2b49ded8842c244bf3729746345a8d8bbe
Rob Stryker <rob@oxbeef.net>
2021-08-11 01:29:51 -0400

    Fix exported members
    Signed-off-by: Rob Stryker <rob@oxbeef.net>

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { ChainNetwork } from "./chain/Chains";
 import { PickleModel } from "./model/PickleModel";
 import * as PickleModelJson from "./model/PickleModelJson";
+import * as JarsAndFarms from './model/JarsAndFarms';
 import { ActiveJarHarvestStats, JarHarvestStats } from "./harvest/JarHarvestResolver";
 
-export { ChainNetwork, PickleModel, PickleModelJson, ActiveJarHarvestStats, JarHarvestStats};
+export { ChainNetwork, PickleModel, PickleModelJson, JarsAndFarms, ActiveJarHarvestStats, JarHarvestStats};
