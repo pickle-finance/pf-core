eccc032c816c310103c02ee1636262816b77b203
bwar <rawblem@gmail.com>
2021-11-22 00:21:46 -0500

    Coinmarketcap api key used too much, hotfix workaround
    Signed-off-by: bwar <rawblem@gmail.com>

--- src/price/CoinMarketCapPriceResolver.ts
+++ src/price/CoinMarketCapPriceResolver.ts
@@ -5,7 +5,8 @@ import fetch from "cross-fetch";
 export class CoinMarketCapPriceResolver extends AbstractPriceResolver implements IPriceResolver{
     protected async fetchPricesBySearchId(searchNames: string[]): Promise<Map<string, number>> {
       const joined = searchNames.join(",");
-      const url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?CMC_PRO_API_KEY=2ba4867e-b50f-44d7-806d-c0c5fde1a8db&slug=" + joined;
+      //const url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?CMC_PRO_API_KEY=2ba4867e-b50f-44d7-806d-c0c5fde1a8db&slug=" + joined;
+      const url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?CMC_PRO_API_KEY=2b734062-7f54-4850-883b-eea8eadbbd51&slug=" + joined;
       const res = await fetch(url).then((x) => x.json());
       const data = res.data;
       const ret = new Map<string,number>();
