ee1c856335e7c35145cfe111f7903f23f59aaa51
mrmard <mistermard@protonmail.com>
2021-12-13 04:41:30 +0800

    fix crv apr for polyJar 3a

--- src/behavior/impl/p3crv.ts
+++ src/behavior/impl/p3crv.ts
@@ -1,18 +1,52 @@
-import { ethers, Signer } from "ethers";
+import { Contract, ethers, Signer } from "ethers";
 import { Provider } from "@ethersproject/providers";
 import { curveThirdPartyGaugeAbi } from "../../Contracts/ABIs/curve-external-gauge.abi";
 import { AssetProjectedApr, JarDefinition } from "../../model/PickleModelJson";
-import { AbstractJarBehavior } from "../AbstractJarBehavior";
+import { AbstractJarBehavior, ONE_YEAR_IN_SECONDS } from "../AbstractJarBehavior";
 import { ChainNetwork } from "../../chain/Chains";
 import { PickleModel } from "../../model/PickleModel";
 import { getCurveRawStats } from "./curve-jar";
 import { Contract as MulticallContract } from "ethers-multicall";
 import erc20Abi from "../../Contracts/ABIs/erc20.json";
 import fetch from "cross-fetch";
-import { formatEther } from "ethers/lib/utils";
+import { formatEther, formatUnits } from "ethers/lib/utils";
 import { getStableswapPriceAddress } from "../../price/DepositTokenPriceUtility";
+import { VvsCroBifi } from "./vvs-cro-bifi";
+import crvStreamerAbi from "../../Contracts/ABIs/crv-reward-streamer.json";
 
 const swap_abi = ["function balances(uint256) view returns(uint256)"];
+const crv_streamer_abi = [
+  {
+    "stateMutability": "view",
+    "type": "function",
+    "name": "reward_data",
+    "inputs": [{ "name": "arg0", "type": "address" }],
+    "outputs": [
+      { "name": "distributor", "type": "address" },
+      { "name": "period_finish", "type": "uint256" },
+      { "name": "rate", "type": "uint256" },
+      { "name": "duration", "type": "uint256" },
+      { "name": "received", "type": "uint256" },
+      { "name": "paid", "type": "uint256" }
+    ]
+  }
+];
+const non_crv_streamer_abi = [
+  {
+    "stateMutability": "view",
+    "type": "function",
+    "name": "period_finish",
+    "inputs": [],
+    "outputs": [{ "name": "", "type": "uint256" }],
+  },
+  {
+    "stateMutability": "view",
+    "type": "function",
+    "name": "reward_rate",
+    "inputs": [],
+    "outputs": [{ "name": "", "type": "uint256" }]
+  }
+]
 
 const aaveContracts = {
   N_COINS: 3,
@@ -39,6 +73,22 @@ const aaveContracts = {
   // sCurveRewards_abi: paaveRewardsabi,
   sCurveRewards_address: "0xBdFF0C27dd073C119ebcb1299a68A6A92aE607F0",
   reward_token: "0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270",
+  rewards: [
+    {
+      stream: "0xC48f4653dd6a9509De44c92beb0604BEA3AEe714",
+      token: "0x172370d5Cd63279eFa6d502DAB29171933a610AF",
+      id: "crv",
+      decimals: 18,
+      abi: crv_streamer_abi,
+    },
+    {
+      stream: "0xBdFF0C27dd073C119ebcb1299a68A6A92aE607F0",
+      token: "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270",
+      id: "matic",
+      decimals: 18,
+      abi: non_crv_streamer_abi
+    },
+  ],
 };
 
 export class PThreeCrv extends AbstractJarBehavior {
@@ -93,7 +143,7 @@ export class PThreeCrv extends AbstractJarBehavior {
     const multicallProvider = model.multicallProviderFor(jar.chain);
     await multicallProvider.init();
 
-    const now = Date.now() / 1000;
+    // const now = Date.now() / 1000;
     const statsAave = await (
       await fetch(
         `https://aave-api-v2.aave.com/data/markets-data/0xd05e3e715d945b59290df0ae8ef85c1bdb684744`,
@@ -101,10 +151,10 @@ export class PThreeCrv extends AbstractJarBehavior {
     ).json();
 
     const lpToken = new MulticallContract(jar.depositToken.addr, erc20Abi);
-    const swap = new MulticallContract(
-      aaveContracts.swap_address,
-      aaveContracts.swap_abi,
-    );
+    // const swap = new MulticallContract(
+    //   aaveContracts.swap_address,
+    //   aaveContracts.swap_abi,
+    // );
 
     const aaveApys: number[] = [];
     aaveContracts.underlying_coins.map(async (coinAddress) => {
@@ -116,33 +166,39 @@ export class PThreeCrv extends AbstractJarBehavior {
     });
 
     const [
-      balance0,
-      balance1,
-      balance2,
-      lpSupply,
+      // balance0,
+      // balance1,
+      // balance2,
+      // lpSupply,
       lpBalance, // Balance staked in gauge
     ] = await multicallProvider.all([
-      swap.balances(0),
-      swap.balances(1),
-      swap.balances(2),
-      lpToken.totalSupply(),
+      // swap.balances(0),
+      // swap.balances(1),
+      // swap.balances(2),
+      // lpToken.totalSupply(),
       lpToken.balanceOf(aaveContracts.gauge_address),
     ]);
-    const scaledBalance0 = balance0 / aaveContracts.coin_precisions[0];
-    const scaledBalance1 = balance1 / aaveContracts.coin_precisions[1];
-    const scaledBalance2 = balance2 / aaveContracts.coin_precisions[2];
+    // const scaledBalance0 = balance0 / aaveContracts.coin_precisions[0];
+    // const scaledBalance1 = balance1 / aaveContracts.coin_precisions[1];
+    // const scaledBalance2 = balance2 / aaveContracts.coin_precisions[2];
 
-    const totalBalance = scaledBalance0 + scaledBalance1 + scaledBalance2;
-    const aaveAPY =
-      aaveApys[0] * (scaledBalance0 / totalBalance) +
-      aaveApys[1] * (scaledBalance1 / totalBalance) +
-      aaveApys[2] * (scaledBalance2 / totalBalance);
+    // const totalBalance = scaledBalance0 + scaledBalance1 + scaledBalance2;
+    // const aaveAPY =
+    //   aaveApys[0] * (scaledBalance0 / totalBalance) +
+    //   aaveApys[1] * (scaledBalance1 / totalBalance) +
+    //   aaveApys[2] * (scaledBalance2 / totalBalance);
 
-    const wmaticRewardsAmount = (await model.priceOf("matic")) * 1042784 * 6; // Multiplied by 6 to annualize the rewards
-    const crvRewardsAmount = (await model.priceOf("crv")) * 796812 * 6;
+    const yearlyRewards = await this.getRewardsAprs(jar, model);
+    const rewardsAprComponents = yearlyRewards.map( reward => {
+      const apr = reward.yearlyRewardsUSD / +formatEther(lpBalance);
+      return this.createAprComponent(reward.id, apr * 100, true);
+    })
 
-    const wmaticAPY = wmaticRewardsAmount / +formatEther(lpBalance);
-    const crvAPY = crvRewardsAmount / +formatEther(lpBalance);
+    // const wmaticRewardsAmount = (await model.priceOf("matic")) * 1042784 * 6; // Multiplied by 6 to annualize the rewards
+    // const crvRewardsAmount = (await model.priceOf("crv")) * 796812 * 6;
+
+    // const wmaticAPY = wmaticRewardsAmount / +formatEther(lpBalance);
+    // const crvAPY = crvRewardsAmount / +formatEther(lpBalance);
 
     const curveRawStats: any = await getCurveRawStats(model, jar.chain);
 
@@ -152,8 +208,59 @@ export class PThreeCrv extends AbstractJarBehavior {
         curveRawStats ? curveRawStats.aave : 0,
         false,
       ),
-      this.createAprComponent("crv", crvAPY * 100, true),
-      this.createAprComponent("matic", wmaticAPY * 100, true),
+      // this.createAprComponent("crv", crvAPY * 100, true),
+      // this.createAprComponent("matic", wmaticAPY * 100, true),
+      ...rewardsAprComponents,
     ]);
+  }
+
+  async getRewardsAprs(jar: JarDefinition, model: PickleModel) {
+    const rewardStreamerMulti = new MulticallContract(
+      aaveContracts.rewards[0].stream,
+      crv_streamer_abi,
+    );
+    
+
+    const yearlyRewards = (await Promise.all(
+      aaveContracts.rewards.map(async (reward) => {
+        var rewardRate: number;
+        var periodFinish: number;
+        const price = model.priceOfSync(reward.token);
+        if (reward.id === "crv"){
+          const rewardStreamer = new Contract(
+            reward.stream,
+            reward.abi,
+            model.providerFor(jar.chain),
+          );
+          const { period_finish: periodFinishBN, rate: rateBN } = await rewardStreamer.reward_data(
+            reward.token,
+          );
+          rewardRate = +formatUnits(rateBN, reward.decimals);
+          periodFinish = +formatUnits(periodFinishBN, 0);
+        } else {
+          const rewardStreamer = new Contract(
+            reward.stream,
+            reward.abi,
+            model.providerFor(jar.chain),
+          );
+          const [periodFinishBN, rateBN] = await Promise.all([
+            rewardStreamer.period_finish(),
+            rewardStreamer.reward_rate(),
+          ])
+          rewardRate = +formatUnits(rateBN, reward.decimals);
+          periodFinish = +formatUnits(periodFinishBN, 0);
+        }        
+        
+        return {
+          token: reward.token,
+          id: reward.id,
+          periodFinish: periodFinish,
+          yearlyRewardsUSD: rewardRate * price * ONE_YEAR_IN_SECONDS,
+        }
+      }),
+    ))
+    .filter(x => x.periodFinish > (Date.now() / 1000));
+
+    return yearlyRewards;
   }
 }
