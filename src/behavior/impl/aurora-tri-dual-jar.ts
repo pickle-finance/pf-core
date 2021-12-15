c40bfbe6225b52e32b2a7b33fa2bef00e29b9c39
Larry the Cucumber <larrythecucumb321@gmail.com>
2021-12-15 13:35:13 -0800

    use getHarvestableUSDDefaultImplementation for aurora dual jars

--- src/behavior/impl/aurora-tri-dual-jar.ts
+++ src/behavior/impl/aurora-tri-dual-jar.ts
@@ -21,50 +21,9 @@ export abstract class AuroraTriDualJar extends AbstractJarBehavior {
     jar: JarDefinition,
     model: PickleModel,
     resolver: Signer | Provider,
-  ): Promise<number> {
-    const strategy = new ethers.Contract(
-      jar.details.strategyAddr,
-      this.strategyAbi,
-      resolver,
-    );
-
-    const triToken = new ethers.Contract(
-      model.address("tri", ChainNetwork.Aurora),
-      erc20Abi,
-      resolver,
-    );
-
-    const auroraToken = new ethers.Contract(
-      model.address("aurora", ChainNetwork.Aurora),
-      erc20Abi,
-      resolver,
-    );
-
-    const [walletTri, walletAurora, triPrice, auroraPrice]: [
-      BigNumber,
-      BigNumber,
-      number,
-      number,
-    ] = await Promise.all([
-      triToken.balanceOf(jar.details.strategyAddr),
-      auroraToken.balanceOf(jar.details.strategyAddr),
-      await model.priceOf("tri"),
-      await model.priceOf("aurora"),
-    ]);
-
-    const res = await strategy.getHarvestable();
-    const pendingTri = res[0];
-    const pendingAurora = res[1];
-
-    const harvestable = pendingTri
-      .add(walletTri)
-      .mul((triPrice * 1e18).toFixed())
-      .div((1e18).toFixed())
-      .add(pendingAurora.add(walletAurora))
-      .mul((auroraPrice * 1e18).toFixed())
-      .div((1e18).toFixed());
-
-    return parseFloat(ethers.utils.formatEther(harvestable));
+  ): Promise<number> {    
+    return this.getHarvestableUSDDefaultImplementation(jar, model, resolver, 
+    ["tri","aurora"], this.strategyAbi);
   }
 
   async getProjectedAprStats(
