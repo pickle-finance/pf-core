989d2f122933e6530bc23be547c15ffba4980739
bwar <rawblem@gmail.com>
2021-12-28 02:31:03 -0500

    Find out which docs are missing
    Signed-off-by: bwar <rawblem@gmail.com>

--- src/docsMain.ts
+++ src/docsMain.ts
@@ -1,11 +1,22 @@
 import { DocsFormat, documentationAssetDefinitionToResult } from './docModel/documentationInterfaces';
 import { ALL_JAR_DOCUMENTATION } from './docModel/docs';
+import { ALL_ASSETS } from './model/JarsAndFarms';
 
 function getDocs(language: string) {
   const docs = [];
   for (let i=0; i < ALL_JAR_DOCUMENTATION.length; i++) {
     // console.log(documentationAssetDefinitionToResult(language, ALL_JAR_DOCUMENTATION[i]));
     docs.push(documentationAssetDefinitionToResult(language, DocsFormat.HTML, ALL_JAR_DOCUMENTATION[i]));
+  }
+
+  for( let i = 0; i < ALL_ASSETS.length; i++ ) {
+    const apiKey = ALL_ASSETS[i]?.details?.apiKey;
+    if( apiKey !== undefined ) {
+      const foundDocs = ALL_JAR_DOCUMENTATION.find((x)=>x.apiKey === apiKey);
+      if( foundDocs === undefined ) {
+        console.log("Docs missing for asset " + apiKey);
+      }
+    }
   }
   return docs;
 }
