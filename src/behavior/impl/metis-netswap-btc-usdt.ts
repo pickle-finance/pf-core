6a0ce0aa81896efd7e5fbadfec475eff10b617c7
amatureApe <amatureApe@gmail.com>
2022-02-25 12:51:15 -0800

    add new getApr logic for netswap. All aprs work well.

--- src/behavior/impl/metis-netswap-btc-usdt.ts
+++ src/behavior/impl/metis-netswap-btc-usdt.ts
@@ -1,26 +1,7 @@
 import { NetswapJar } from "./metis-netswap-jar";
-import { Provider } from "@ethersproject/providers";
-import { Signer } from "ethers";
-import { PickleModel } from "../..";
-import { JarDefinition } from "../../model/PickleModelJson";
-import { multiSushiStrategyAbi } from "../../Contracts/ABIs/multi-sushi-strategy.abi";
 
 export class NetswapBtcUsdt extends NetswapJar {
   constructor() {
     super();
-  }
-
-  async getHarvestableUSD(
-    jar: JarDefinition,
-    model: PickleModel,
-    resolver: Signer | Provider,
-  ): Promise<number> {
-    return this.getHarvestableUSDDefaultImplementation(
-      jar,
-      model,
-      resolver,
-      ["nett", "btc"],
-      multiSushiStrategyAbi,
-    );
   }
 }
