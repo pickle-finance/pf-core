1fe7cc10de343f065ab42052df4ce10abb7afc45
amatureApe <amatureApe@gmail.com>
2022-03-09 18:58:52 -0800

    rosepool apy calcs working

--- src/behavior/impl/aurora-rose-ustpool.ts
+++ src/behavior/impl/aurora-rose-ustpool.ts
@@ -3,11 +3,12 @@ import { PickleModel } from "../../model/PickleModel";
 import { AssetAprComponent, JarDefinition } from "../../model/PickleModelJson";
 import { Contract as MultiContract } from "ethers-multicall";
 import roseFarmAbi from "../../Contracts/ABIs/rose-farm.json";
-import gaugeAbi from "../../Contracts/ABIs/curve-factory-gauge.json";
+import poolAbi from "../../Contracts/ABIs/pool.json";
 import { formatEther } from "ethers/lib/utils";
+import { ethers } from "ethers";
 import { ONE_YEAR_SECONDS } from "../JarBehaviorResolver";
 import { createAprComponentImpl } from "../../behavior/AbstractJarBehavior";
-import { getStableswapPrice } from "../../price/DepositTokenPriceUtility";
+import { getStableswapPriceAddress } from "../../price/DepositTokenPriceUtility";
 
 const rewarderAddress = "0x56DE5E2c25828040330CEF45258F3FFBc090777C";
 const poolAddress = "0x8fe44f5cce02D5BE44e3446bBc2e8132958d22B8";
@@ -18,28 +19,26 @@ export class RoseUstpool extends RoseJar {
     super(rewarderAddress);
   }
 
-  // async getDepositTokenPrice(
-  //   asset: JarDefinition,
-  //   model: PickleModel,
-  // ): Promise<number> {
-  //   return getStableswapPrice(asset, model);
-  // }
+  async getDepositTokenPrice(
+    asset: JarDefinition,
+    model: PickleModel,
+  ): Promise<number> {
+    return getStableswapPriceAddress(poolAddress, asset, model);;
+  }
 
   async calculateRoseAPY(
     jar: JarDefinition,
     model: PickleModel,
   ): Promise<AssetAprComponent[]> {
-    const multicallRosePool = new MultiContract(poolAddress, gaugeAbi);
-    console.log("PING", multicallRosePool)
-    const pricePerToken = await model.callMulti(
-      [
-        () => multicallRosePool.get_virtual_price()
-      ],
-      jar.chain,
-    );
-
-    // const pricePerToken = model.priceOfSync(jar.depositToken.addr, jar.chain);
-
+    const multicallRosePool = new MultiContract(poolAddress, poolAbi);
+    const pricePerToken = (
+      await model.callMulti(
+        [
+          () => multicallRosePool.get_virtual_price()
+        ],
+        jar.chain,
+      )
+    ).map((x) => parseFloat(ethers.utils.formatUnits(x)))
 
     const multicallRoseRewards = new MultiContract(
       rewarderAddress,
@@ -52,24 +51,30 @@ export class RoseUstpool extends RoseJar {
       ],
       jar.chain,
     );
+    console.log("rewardData", rewardData)
+    console.log("totalSupplyBN", totalSupplyBN);
 
     const totalSupply = parseFloat(formatEther(totalSupplyBN));
+    console.log("totalSupply", totalSupply)
 
     const roseRewardsPerYear =
       parseFloat(formatEther(rewardData[3])) * ONE_YEAR_SECONDS;
+    console.log("roseRewardsPerYear", roseRewardsPerYear);
 
     const valueRewardedPerYear =
       model.priceOfSync("rose", jar.chain) * roseRewardsPerYear;
+    console.log("valueRewardedPerYear", valueRewardedPerYear);
 
 
-
-    const totalValueStaked = totalSupply * pricePerToken;
-    const roseAPY = 100 * (valueRewardedPerYear / totalValueStaked);
+    const totalValueStaked = (totalSupply * pricePerToken);
+    console.log("totalValueStaked", totalValueStaked);
+    const roseAPY = (valueRewardedPerYear / totalValueStaked);
+    console.log("roseAPY", roseAPY);
 
     return [
       createAprComponentImpl(
         "rose",
-        roseAPY,
+        roseAPY * 100,
         true,
         0.9,
       ),
