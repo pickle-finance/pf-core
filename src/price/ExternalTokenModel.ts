0b44cb231ca11f10eac4a3ec9c5aca733c2c04ba
Cipio <Cipio@Pickle.finance>
2022-05-14 08:34:00 -0500

    Add description

--- src/price/ExternalTokenModel.ts
+++ src/price/ExternalTokenModel.ts
@@ -363,7 +363,7 @@ export class ExternalTokenModel {
     this.addToken(
       ChainNetwork.Ethereum,
       "temple",
-      "temple",
+      null,
       "temple",
       "0x470EBf5f030Ed85Fc1ed4C2d36B9DD02e77CF1b7".toLowerCase(),
       18,
