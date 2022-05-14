30ccea84a6ed364419cbfa6ed0a30b99afcd2804
Cipio <Cipio@Pickle.finance>
2022-05-13 23:07:12 -0500

    Add FRAX/TEMPLE

--- src/model/JarsAndFarms.ts
+++ src/model/JarsAndFarms.ts
@@ -1857,6 +1857,35 @@ export const JAR_STAR_USDT: JarDefinition = {
 };
 JAR_DEFINITIONS.push(JAR_STAR_USDT);
 
+export const JAR_UNIV2_FRAX_TEMPLE: JarDefinition = {
+  type: AssetType.JAR,
+  id: "pJar U3j",
+  contract: "0x4eB6e19c043513db1451eEe57F0d58ebea2C6150",
+  startBlock: 14194675,
+  depositToken: {
+    addr: "0x6021444f1706f15465bEe85463BCc7d7cC17Fc03",
+    name: "UniV2 FRAX/TEMPLE",
+    link: "https://app.uniswap.org/#/add/0x853d955aCEf822Db058eb8505911ED77F175b99e/0x6B175474E89094C44Da98b954EedeAC495271d0F/500",
+    components: ["temple", "frax"],
+    style: { erc20: false },
+  },
+  rewardTokens: ["fxs", "temple"],
+  enablement: AssetEnablement.DEV,
+  chain: ChainNetwork.Ethereum,
+  protocol: AssetProtocol.UNISWAP,
+  details: {
+    controller: "0x6847259b2B3A4c17e7c43C54409810aF48bA5210",
+    apiKey: "UNIV2-FRAX-TEMPLE",
+    harvestStyle: HarvestStyle.PASSIVE,
+  },
+  farm: {
+    farmAddress: "0xb7a632f93e3cbcc9444114bcd162b704e07355a9",
+    farmNickname: "pUNIv2 FRAX/TEMPLE",
+    farmDepositTokenName: "pUNIv2 FRAX/TEMPLE",
+  },
+};
+JAR_DEFINITIONS.push(JAR_UNIV2_FRAX_TEMPLE);
+
 // Polygon
 
 export const JAR_COMETH_USDC_WETH: JarDefinition = {
