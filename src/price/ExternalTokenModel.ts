5401b46b392593ae7cf33542f1c43c0a128717d9
Larry the Cucumber <larrythecucumber321@gmail.com>
2022-10-26 20:55:21 -0700

    fix: correct stg/gmx token addresses

--- src/price/ExternalTokenModel.ts
+++ src/price/ExternalTokenModel.ts
@@ -923,7 +923,7 @@ export class ExternalTokenModel {
       "stg",
       null,
       "stargate-finance",
-      "0xfc5a1a6eb076a2c7ad06ed22c90d7e710e35ad0a".toLowerCase(),
+      "0x6694340fc020c5E6B96567843da2df01b2CE1eb6".toLowerCase(),
       18,
       ExternalTokenFetchStyle.ID,
     );
@@ -932,7 +932,7 @@ export class ExternalTokenModel {
       "gmx",
       null,
       "gmx",
-      "0x6694340fc020c5E6B96567843da2df01b2CE1eb6".toLowerCase(),
+      "0xfc5a1a6eb076a2c7ad06ed22c90d7e710e35ad0a".toLowerCase(),
       18,
       ExternalTokenFetchStyle.ID,
     );
